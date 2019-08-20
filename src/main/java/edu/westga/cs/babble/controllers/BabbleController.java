b10acc093502559f137b177a009f8e7431fda829
Chris Jones <cjones92@my.westga.edu>
2019-08-20 15:25:25 -0400

    Added imports and variables to GUI classes

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -3,6 +3,18 @@
  */
 package edu.westga.cs.babble.controllers;
 
+import java.text.Format;
+
+import edu.westga.cs.babble.model.PlayedWord;
+import edu.westga.cs.babble.model.Tile;
+import edu.westga.cs.babble.model.TileBag;
+import edu.westga.cs.babble.model.TileGroup;
+import edu.westga.cs.babble.model.TileRack;
+import javafx.beans.property.IntegerProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 /**
  * @author Chris Jones
  * @version August 20, 2019
@@ -12,9 +24,25 @@ package edu.westga.cs.babble.controllers;
 public class BabbleController {
 	
 	private WordDictionary babbleDictionary;
+	private PlayedWord babblePlayedWord;
+	private TileBag babbleTileBag;
+	private TileRack babbletileRack;
+	private IntegerProperty babbleScoreProperty;
+    private Button resetButton;
+    private TextField scoreText;
+	
 	
 	public BabbleController() {
 		this.babbleDictionary = new WordDictionary();
+		this.babblePlayedWord = new PlayedWord();
+		this.babbleTileBag = new TileBag();
+		this.babbletileRack = new TileRack();
+		this.resetButton = new Button();
+		this.scoreText = new TextField();
 	}
 
+	
+	public void setScore() {
+	
+		}
 }
