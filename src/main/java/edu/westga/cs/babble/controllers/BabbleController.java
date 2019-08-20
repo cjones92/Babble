7b8d0affa8668107aabb83e119c009608889b520
Chris Jones <cjones92@my.westga.edu>
2019-08-20 12:51:13 -0400

    Created BabbleController class

---
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.westga.cs.babble.controllers;
+
+/**
+ * @author Chris Jones
+ * @version August 20, 2019
+ * Controls behavior of the Babble GUI
+ *
+ */
+public class BabbleController {
+	
+	private WordDictionary babbleDictionary;
+	
+	public BabbleController() {
+		this.babbleDictionary = new WordDictionary();
+	}
+
+}
