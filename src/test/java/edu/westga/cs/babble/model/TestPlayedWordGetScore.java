c4340f916936ed1abf117b70ec59c78129e270d6
Chris Jones <cjones92@my.westga.edu>
2019-09-04 11:49:38 -0400

    Altered TestPlayedWordGetScore to use setUp method

--- src/test/java/edu/westga/cs/babble/model/TestPlayedWordGetScore.java
+++ src/test/java/edu/westga/cs/babble/model/TestPlayedWordGetScore.java
@@ -5,7 +5,10 @@ package edu.westga.cs.babble.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs.babble.controllers.WordDictionary;
 
 /**
  * @author Chris Jones
@@ -13,48 +16,52 @@ import org.junit.jupiter.api.Test;
  *
  */
 class TestPlayedWordGetScore {
+	private PlayedWord testPlayedWord;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		this.testPlayedWord = new PlayedWord();
+	}
 
 	@Test
 	public void emptyWordShouldHaveScoreOfZero() {
-		PlayedWord testPlayedWord = new PlayedWord();
-		assertEquals(0, testPlayedWord.getScore());
+		assertEquals(0, this.testPlayedWord.getScore());
 	}
 	
 	@Test
 	public void scoreAOneTileWord() {
-		PlayedWord testPlayedWord = new PlayedWord();
+	
 		Tile testTileE = new Tile('E');
 		Tile testTileA = new Tile('A');
 		Tile testTileT = new Tile('T');
-		testPlayedWord.append(testTileE);
-		testPlayedWord.append(testTileA);
-		testPlayedWord.append(testTileT);
-		assertEquals(3, testPlayedWord.getScore());
+		this.testPlayedWord.append(testTileE);
+		this.testPlayedWord.append(testTileA);
+		this.testPlayedWord.append(testTileT);
+		assertEquals(3, this.testPlayedWord.getScore());
 	}
 	
 	@Test
 	public void scoreAWordWithMultipleDifferingTiles() {
-		PlayedWord testPlayedWord = new PlayedWord();
+		
 		Tile testTileB = new Tile('B');
 		Tile testTileO = new Tile('O');
 		Tile testTileG = new Tile('G');
-		testPlayedWord.append(testTileB);
-		testPlayedWord.append(testTileO);
-		testPlayedWord.append(testTileG);
-		assertEquals(6, testPlayedWord.getScore());
+		this.testPlayedWord.append(testTileB);
+		this.testPlayedWord.append(testTileO);
+		this.testPlayedWord.append(testTileG);
+		assertEquals(6, this.testPlayedWord.getScore());
 	}
 	
 	@Test
 	public void scoreAWordContainingDuplicateTiles() {
-		PlayedWord testPlayedWord = new PlayedWord();
 		Tile testTileE = new Tile('E');
 		Tile testTileG = new Tile('G');
 		Tile testTileGDuplicate = new Tile('G');
-		testPlayedWord.append(testTileE);
-		testPlayedWord.append(testTileG);
-		testPlayedWord.append(testTileGDuplicate);
+		this.testPlayedWord.append(testTileE);
+		this.testPlayedWord.append(testTileG);
+		this.testPlayedWord.append(testTileGDuplicate);
 		
-		assertEquals(5, testPlayedWord.getScore());
+		assertEquals(5, this.testPlayedWord.getScore());
 	}
 	
 
