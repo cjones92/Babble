ef470cc0bc8058da9395d12bdc692c6d73ed93da
Chris Jones <cjones92@my.westga.edu>
2019-09-04 11:57:14 -0400

    Altered TestTileBagDrawTile to use setUp method

--- src/test/java/edu/westga/cs/babble/model/TestTileBagDrawTile.java
+++ src/test/java/edu/westga/cs/babble/model/TestTileBagDrawTile.java
@@ -11,7 +11,10 @@ import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs.babble.controllers.WordDictionary;
 
 /**
  * @author Chris Jones
@@ -19,43 +22,47 @@ import org.junit.jupiter.api.Test;
  *
  */
 class TestTileBagDrawTile {
+	private TileBag testTileBag;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		this.testTileBag = new TileBag();
+	}
 
 	@Test
 	public void canDrawAllFiles() {
-		TileBag testTileBag = new TileBag();
+		
 		for (int index = 0; index < 98; index++) {
 			try {
-				testTileBag.drawTile();
+				this.testTileBag.drawTile();
 			} catch (EmptyTileBagException e) {
-				// TODO Auto-generated catch block
+				
 				e.printStackTrace();
 			}
 		}
 		
-		assertEquals(true, testTileBag.isEmpty());
+		assertEquals(true, this.testTileBag.isEmpty());
 
 	}
 	
 	@Test
 	public void canNotDrawTooManyTiles() {
 		
-		
-		TileBag testTileBag = new TileBag();
 		for (int index = 0; index < 98; index++) {
 			try {
-				testTileBag.drawTile();
+				this.testTileBag.drawTile();
 			} catch (EmptyTileBagException e) {
 				e.printStackTrace();
 			}
 		}
 		
 		Assertions.assertThrows(EmptyTileBagException.class, () -> {
-		    testTileBag.drawTile(); });		
+		    this.testTileBag.drawTile(); });		
 	}
 	
 	@Test 
 	public void hasProperTileDistribution() {
-		TileBag testTileBag = new TileBag();
+
 		ArrayList<Character> tiles = new ArrayList<Character>();
 		ArrayList<Character> letters = new ArrayList<Character>( Arrays.asList('A','A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
 				'B', 'B', 'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E','E', 'E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I', 'I', 'I', 'I','I',
@@ -65,7 +72,7 @@ class TestTileBagDrawTile {
 				
 				for (int index = 0; index < 98; index++) {
 			try {
-				char pulledTile = testTileBag.drawTile().getLetter();
+				char pulledTile = this.testTileBag.drawTile().getLetter();
 				tiles.add(pulledTile);
 				
 			} catch (EmptyTileBagException e) {
