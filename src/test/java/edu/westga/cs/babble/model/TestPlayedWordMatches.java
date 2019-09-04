9495ff4a663cc3e83b2161353df3af8cba02a4d7
Chris Jones <cjones92@my.westga.edu>
2019-09-04 11:53:07 -0400

    Altered TestPlayedWordMatches to use setUp method

--- src/test/java/edu/westga/cs/babble/model/TestPlayedWordMatches.java
+++ src/test/java/edu/westga/cs/babble/model/TestPlayedWordMatches.java
@@ -6,7 +6,10 @@ package edu.westga.cs.babble.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs.babble.controllers.WordDictionary;
 
 /**
  * @author Chris Jones
@@ -14,86 +17,91 @@ import org.junit.jupiter.api.Test;
  *
  */
 class TestPlayedWordMatches {
+	private PlayedWord testPlayedWord;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		this.testPlayedWord = new PlayedWord();
+	}
 
 	@Test
 	public void hasTilesForAMultipleLetterWord()  {
-		PlayedWord testPlayedWord = new PlayedWord();
+		
 		Tile testTileE = new Tile('E');
 		Tile testTileA = new Tile('A');
 		Tile testTileT = new Tile('T');
-		testPlayedWord.append(testTileE);
-		testPlayedWord.append(testTileA);
-		testPlayedWord.append(testTileT);
+		this.testPlayedWord.append(testTileE);
+		this.testPlayedWord.append(testTileA);
+		this.testPlayedWord.append(testTileT);
 		
-		assertEquals(true, testPlayedWord.matches("EAT"));
+		assertEquals(true, this.testPlayedWord.matches("EAT"));
 	}
 	
 	@Test
 	public void hasTilesForASingleLetterWord() {
-		PlayedWord testPlayedWord = new PlayedWord();
-		Tile testTileA = new Tile('A');
-		testPlayedWord.append(testTileA);
 		
-		assertEquals(true, testPlayedWord.matches("A"));
+		Tile testTileA = new Tile('A');
+		this.testPlayedWord.append(testTileA);
+		
+		assertEquals(true, this.testPlayedWord.matches("A"));
 	}
 	
 	@Test
 	public void cannotMatchWordWhenTilesAreScrambled() {
-		PlayedWord testPlayedWord = new PlayedWord();
+		
 		Tile testTileT = new Tile('T');
 		Tile testTileA = new Tile('A');
 		Tile testTileG = new Tile('G');
-		testPlayedWord.append(testTileG);
-		testPlayedWord.append(testTileT);
-		testPlayedWord.append(testTileA);
+		this.testPlayedWord.append(testTileG);
+		this.testPlayedWord.append(testTileT);
+		this.testPlayedWord.append(testTileA);
 		
-		assertEquals(false, testPlayedWord.matches("EAT"));
+		assertEquals(false, this.testPlayedWord.matches("EAT"));
 	}
 	
 	@Test
 	public void cannotMatchWordIfInsufficientTiles() {
-		PlayedWord testPlayedWord = new PlayedWord();
+		
 		Tile testTileE = new Tile('E');
 		Tile testTileG = new Tile('G');
 		
-		testPlayedWord.append(testTileE);
-		testPlayedWord.append(testTileG);
-		assertEquals(false, testPlayedWord.matches("EGG"));
+		this.testPlayedWord.append(testTileE);
+		this.testPlayedWord.append(testTileG);
+		assertEquals(false, this.testPlayedWord.matches("EGG"));
 	}
 	
 	@Test
 	public void canMatchWordWithDuplicateLetters() {
-		PlayedWord testPlayedWord = new PlayedWord();
+
 		Tile testTileE = new Tile('E');
 		Tile testTileG = new Tile('G');
 		Tile testTileGDuplicate = new Tile('G');
-		testPlayedWord.append(testTileE);
-		testPlayedWord.append(testTileG);
-		testPlayedWord.append(testTileGDuplicate);
+		this.testPlayedWord.append(testTileE);
+		this.testPlayedWord.append(testTileG);
+		this.testPlayedWord.append(testTileGDuplicate);
 		
-		assertEquals(true, testPlayedWord.matches("EGG"));
+		assertEquals(true, this.testPlayedWord.matches("EGG"));
 	}
 	
 	@Test
 	public void nonEmptyWordShouldNotMatchEmptyText() {
-		PlayedWord testPlayedWord = new PlayedWord();
+		
 		Tile testTileA = new Tile('A');
-		testPlayedWord.append(testTileA);
-		assertEquals(false, testPlayedWord.matches(""));
+		this.testPlayedWord.append(testTileA);
+		assertEquals(false, this.testPlayedWord.matches(""));
 	}
 	
 	@Test
 	public void emptyWordShouldNotMatchEmptyText() {
-		PlayedWord testPlayedWord = new PlayedWord();
-		assertEquals(false, testPlayedWord.matches(""));
+		assertEquals(false, this.testPlayedWord.matches(""));
 	}
 	
 	@Test
 	public void shouldNotAllowNull() {
-		PlayedWord testPlayedWord = new PlayedWord();
+		
 		
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-		    testPlayedWord.matches(null); });
+		    this.testPlayedWord.matches(null); });
 		
 	}
 
